fba71b47595bd940006fdc59d4b74f36f5a29bcb
ZER0A0 <105021041@live.asia.edu.tw>
2016-10-24 15:52:22 +0800

    WEEK05 EX 繳交

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,13 +2,25 @@
 /*
  * Topic: 讓使用者輸入一正整數 n，並輸出 2^1+2^2+2^3+...+2^n 的值。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021041 楊建宸
  */
 
+import java.util.Scanner;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("輸入正整數：");
+		int n = scn.nextInt();
+		int m = 1;
+		int a = 1;
+		int t = 0;
+		
+		while(m <= n)
+		{a = 2 * a ; m = m + 1 ; t = t + a;}
+		System.out.print(t);
 
 	}
 
--- src/ex/ex06.java
+++ src/ex/ex06.java
@@ -2,14 +2,18 @@
 /*
  * Topic: 某個陣列中含有所有學生的學號，但並沒有按照大小次序排列。請寫一程式依學號大小排列並將結果輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021041 楊建宸
  */
 
+import java.util.Scanner;
 public class ex06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		
+		
+		
 	}
 
 }
